b3000c0350dfbfcde30394cc4d302267ebe5aa11
edwarvilchez <edwarvilchez1977@gmail.com>
2025-06-24 15:15:00 -0400

    Configuracion inicial del proyecto

---
+++ src/main/java/com/codigorupestre/servlets/Saludos.java
@@ -0,0 +1,45 @@
+package com.codigorupestre.servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+/**
+ * Servlet implementation class Saludos
+ */
+@WebServlet("/Saludos")
+public class Saludos extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Saludos() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String nombre = "Edwar 'eddiemonster' Vilchez";
+		int edad = 47;
+		String pais = "Venezuela";
+		
+		request.setAttribute("Nombre", nombre);
+		request.setAttribute("Edad", edad);
+		request.setAttribute("País", pais);
+		request.setAttribute("Creación", LocalDateTime.now().toString());
+		
+		request.getRequestDispatcher("/saludos.jsp").forward(request, response);
+	}
+
+	
+
+}
---
+++ src/main/webapp/saludos.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Saludos JSP</title>
+</head>
+<body>
+
+	<h2>Eterno Aprendíz</h2>
+	<p>Nombre: ${nombre}</p>
+	<p>Edad: ${edad} </p>
+	<p>País: ${pais}</p>
+	<p>Creación: ${creacion}</p>
+
+</body>
+</html>
